6fdd84a9042eca03aac36c5d2c97bacac8b730ac
Alexander Carlson <acarlson@e1z3r1p21.42.us.org>
2019-01-21 10:57:28 -0800

    Added function stuff

--- day00/ex02/Account.class.cpp
+++ day00/ex02/Account.class.cpp
@@ -6,16 +6,63 @@
 //   By: acarlson <marvin@42.fr>                    +#+  +:+       +#+        //
 //                                                +#+#+#+#+#+   +#+           //
 //   Created: 2019/01/21 09:02:13 by acarlson          #+#    #+#             //
-//   Updated: 2019/01/21 09:21:45 by acarlson         ###   ########.fr       //
+//   Updated: 2019/01/21 10:56:58 by acarlson         ###   ########.fr       //
 //                                                                            //
 // ************************************************************************** //
 
 #include "Account.class.hpp"
 
-Account::Account()
+Account::Account(int initial_deposit)
 {
+	(void)initial_deposit;
 }
 
 Account::~Account()
+{
+}
+
+
+static int	getNbAccounts( void )
+{
+	return (0);
+}
+
+static int	getTotalAmount( void )
+{
+	return (0);
+}
+
+static int	getNbDeposits( void )
+{
+	return (0);
+}
+
+static int	getNbWithdrawals( void )
+{
+	return (0);
+}
+
+static void	displayAccountsInfos( void );
+
+void	makeDeposit( int deposit )
+{
+	(void)deposit;
+}
+bool	makeWithdrawal( int withdrawal )
+{
+	(void)withdrawal;
+	return (false);
+}
+
+int		checkAmount( void ) const
+{
+	return (0);
+}
+
+void	displayStatus( void ) const
+{
+}
+
+static void	_displayTimestamp( void )
 {
 }
