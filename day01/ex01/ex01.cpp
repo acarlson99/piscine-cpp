e30443179f192249a230cbb9db5e646b0ed281b6
Alexander Carlson <acarlson@e1z3r2p2.42.us.org>
2019-01-22 10:01:16 -0800

    Copy pasted from pdf

--- day01/ex01/ex01.cpp
+++ day01/ex01/ex01.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 #include <string>
 
-void		memoryLeak()
+void memoryLeak()
 {
-	std::string*		panthere = new std::string("String panthere");
-
+	std::string* panthere = new std::string("String panthere");
 	std::cout << *panthere << std::endl;
 	delete panthere;
 }
