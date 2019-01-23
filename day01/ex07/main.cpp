9e6ec1bbef0fb1394160615da6935dc635caff40
Alexander Carlson <acarlson@e1z3r2p8.42.us.org>
2019-01-22 20:19:57 -0800

    Workin I think

--- day01/ex07/main.cpp
+++ day01/ex07/main.cpp
@@ -6,7 +6,7 @@
 //   By: acarlson <marvin@42.fr>                    +#+  +:+       +#+        //
 //                                                +#+#+#+#+#+   +#+           //
 //   Created: 2019/01/22 18:44:35 by acarlson          #+#    #+#             //
-//   Updated: 2019/01/22 19:05:53 by acarlson         ###   ########.fr       //
+//   Updated: 2019/01/22 20:19:16 by acarlson         ###   ########.fr       //
 //                                                                            //
 // ************************************************************************** //
 
@@ -21,14 +21,29 @@ int main(int argc, char **argv)
 		std::cerr << "Usage: " << argv[0] << " filename str1 str2" << std::endl;
 		return (1);
 	}
-	std::ifstream inFile;
-	inFile.open(argv[1]);
-	if (!inFile)
+	std::ifstream ifs(argv[1]);
+	std::string s = argv[2];
+	std::string r = argv[3];
+	if (s.length() < 1)
 	{
-		std::cerr << "Unable to open file" << std::endl;
+		std::cerr << "OwO that's a tiny widdwe pawametew" << std::endl;
 		return (1);
 	}
-	char buf[5000000];
-	while (inFile >> buf)
-		std::cout << buf;
+	char *a = strcat(argv[1] , ".replace");
+	std::ofstream ofs(a);
+	std::string readout;
+	while (getline(ifs, readout))
+	{
+		for (int i = 0; readout[i]; i++)
+		{
+			if (readout.substr(i, s.length()) == s)
+			{
+				ofs << r;
+				i += s.length() - 1;
+			}
+			else
+				ofs << (char)readout[i];
+		}
+		ofs << std::endl;
+	}
 }
