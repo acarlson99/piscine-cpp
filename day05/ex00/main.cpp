eb7c9989187f6269b63bd234e3a72f1176661ac7
Alexander Carlson <acarlson@e1z3r2p8.42.us.org>
2019-01-28 13:59:07 -0800

    d5

---
+++ day05/ex00/main.cpp
@@ -0,0 +1,50 @@
+// ************************************************************************** //
+//                                                                            //
+//                                                        :::      ::::::::   //
+//   main.cpp                                           :+:      :+:    :+:   //
+//                                                    +:+ +:+         +:+     //
+//   By: acarlson <marvin@42.fr>                    +#+  +:+       +#+        //
+//                                                +#+#+#+#+#+   +#+           //
+//   Created: 2019/01/28 11:44:36 by acarlson          #+#    #+#             //
+//   Updated: 2019/01/28 13:37:56 by acarlson         ###   ########.fr       //
+//                                                                            //
+// ************************************************************************** //
+
+#include "Bureaucrat.hpp"
+
+int main()
+{
+	Bureaucrat jim = Bureaucrat("Jim", 20);
+
+	Bureaucrat tim;
+
+	tim = jim;
+
+	std::cout << jim << std::endl;
+	std::cout << tim << std::endl;
+	try {
+		tim = Bureaucrat("Thomas", 0);
+	}
+	catch (Bureaucrat::GradeTooHighException &e) {
+		std::cout << "Grade too high" << std::endl;
+	}
+	catch (Bureaucrat::GradeTooLowException &e) {
+		std::cout << "Grade too low" << std::endl;
+	}
+	try {
+		tim = Bureaucrat("Thomas", 149);
+		std::cout << tim << " created" << std::endl;
+		tim.decGrade();
+		std::cout << tim << " grade incremented" << std::endl;
+		tim.decGrade();
+		std::cout << tim << " grade decremented" << std::endl;
+		tim.decGrade();
+		std::cout << tim << " grade decremented" << std::endl;
+	}
+	catch (Bureaucrat::GradeTooHighException &e) {
+		std::cout << "Grade too high" << std::endl;
+	}
+	catch (Bureaucrat::GradeTooLowException &e) {
+		std::cout << "Grade too low" << std::endl;
+	}
+}
