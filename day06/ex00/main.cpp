a52b53a4fad7af0ed25b3f7a2de426f2188f4aaf
Alexander Carlson <acarlson@e1z3r2p8.42.us.org>
2019-01-29 15:49:16 -0800

    m

--- day06/ex00/main.cpp
+++ day06/ex00/main.cpp
@@ -6,11 +6,13 @@
 /*   By: acarlson <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/29 11:52:08 by acarlson          #+#    #+#             */
-//   Updated: 2019/01/29 13:36:25 by acarlson         ###   ########.fr       //
+//   Updated: 2019/01/29 15:17:40 by acarlson         ###   ########.fr       //
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <string>
+#include <math.h>
 
 /*
 ** static - Similar to c casting I think
@@ -28,7 +30,27 @@ int main(int argc, char **argv)
 	}
 	double		num;
 	std::string	s = argv[1];
-	num = atof(argv[1]);
-	std::cout << num << std::endl;
-	std::cout << (static_cast<char>(num)) << std::endl;
+	if (s.length() == 1 && !(s[0] >= '0' && s[0] <= '9'))
+		num = static_cast<double>(s[0]);
+	else
+		num = atof(argv[1]);
+	char	numc = static_cast<char>(num);
+	std::cout << "char: ";
+	if ((s == "nan") || (s == "-inf") || (s == "+inf") || s == "inf")
+		std::cout << "impossible" << std::endl;
+	else if (numc < 040)
+		std::cout << "Non displayable" << std::endl;
+	else
+		std::cout << numc << std::endl;
+	int		numi = static_cast<int>(num);
+	std::cout << "int: ";
+	if ((s == "nan") || (s == "-inf") || (s == "+inf"))
+		std::cout << "impossible" << std::endl;
+	else
+		std::cout << numi << std::endl;
+	float	numf = static_cast<float>(num);
+	std::cout << "float: ";
+	std::cout << numf << (numi == numf ? ".0" : "") << "f" << std::endl;
+	std::cout << "double: ";
+	std::cout << num << (numi == numf ? ".0" : "") << std::endl;
 }
