dcf51b8bc5c8cd44bc9c1f577317def481768e56
Alexander Carlson <acarlson@e1z3r2p8.42.us.org>
2019-01-29 13:45:10 -0800

    day 6

--- day06/ex00/main.cpp
+++ day06/ex00/main.cpp
@@ -6,15 +6,29 @@
 /*   By: acarlson <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/29 11:52:08 by acarlson          #+#    #+#             */
-/*   Updated: 2019/01/29 12:03:28 by acarlson         ###   ########.fr       */
+//   Updated: 2019/01/29 13:36:25 by acarlson         ###   ########.fr       //
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
+/*
+** static - Similar to c casting I think
+** dynamic - Evaluated at runtime
+** reinterpret - Just reinterpret the raw bits
+** const - can remove const qualifier DO NOT USE THIS TOO MUCH
+*/
+
 int main(int argc, char **argv)
 {
-	(void)argc;
-	(void)argv;
-	std::cout << (0.0 / 0.0) << std::endl;
+	if (argc != 2)
+	{
+		std::cout << "Invalid number of parameters" << std::endl;
+		return (0);
+	}
+	double		num;
+	std::string	s = argv[1];
+	num = atof(argv[1]);
+	std::cout << num << std::endl;
+	std::cout << (static_cast<char>(num)) << std::endl;
 }
