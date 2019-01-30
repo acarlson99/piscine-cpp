aac2ae873cf33f4fd32b4d50a8a0a082b2e56c8f
Alexander Carlson <acarlson@e1z2r5p29.42.us.org>
2019-01-29 21:15:27 -0800

    neat

--- day06/ex01/main.cpp
+++ day06/ex01/main.cpp
@@ -1,14 +1,14 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: acarlson <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/01/29 16:11:38 by acarlson          #+#    #+#             */
-//   Updated: 2019/01/29 17:21:19 by acarlson         ###   ########.fr       //
-/*                                                                            */
-/* ************************************************************************** */
+// ************************************************************************** //
+//                                                                            //
+//                                                        :::      ::::::::   //
+//   main.cpp                                           :+:      :+:    :+:   //
+//                                                    +:+ +:+         +:+     //
+//   By: acarlson <marvin@42.fr>                    +#+  +:+       +#+        //
+//                                                +#+#+#+#+#+   +#+           //
+//   Created: 2019/01/29 18:49:06 by acarlson          #+#    #+#             //
+//   Updated: 2019/01/29 21:13:15 by acarlson         ###   ########.fr       //
+//                                                                            //
+// ************************************************************************** //
 
 #include <random>
 #include <iostream>
@@ -30,25 +30,14 @@ void		*serialize( void )
 	int				m = 0;
 	char	*n = new char[sizeof(int) + sizeof(char) * 8 * 2];
 	for (; i < 8; i++)
-	{
 		n[i] = g_str[mt_rand() % g_str.length()];
-		std::cout << static_cast<char>(n[i]);
-	}
-	std::cout << std::endl;
 	for (; i < sizeof(int) + 8; i++)
 	{
 		n[i] = mt_rand() % CHAR_MAX;
-		m += n[i] << ABS(8 - 1);
-		std::cout << static_cast<int>(n[i]) << " ";
+		m |= n[i] << ABS(i - 8);
 	}
-	std::cout << std::endl;
 	for (; i < 8 + 8 + sizeof(int); i++)
-	{
 		n[i] = g_str[mt_rand() % g_str.length()];
-		std::cout << static_cast<char>(n[i]);
-	}
-	std::cout << std::endl;
-	std::cout << "M " << m << std::endl;
 	return (n);
 }
 
@@ -63,18 +52,36 @@ Data		*deserialize( void *raw)
 	Data		*d = new Data;
 	for (; i < 8; i++)
 		s1[i] = s[i];
-	for (; i < sizeof(int) + 8; i++)
-		n += s[i] << ABS(8 - i);
+	n = *reinterpret_cast<int *>(&s[i]);
+	i += sizeof(int);
 	for (; i < 8 + 8 + sizeof(int); i++)
 		s2[a++] = s[i];
-	std::cout << "N " << n << std::endl;
-	std::cout << "s1 " << s1 << std::endl << "s2 " << s2 << std::endl;
 	d->s1 = s1;
+	d->s2 = s2;
+	d->n = n;
 	return (d);
 }
 
 int main()
 {
-	deserialize(serialize());
+	Data *d = deserialize(serialize());
+	std::cout << d->s1 << std::endl;
+	std::cout << d->s2 << std::endl;
+	std::cout << d->n << std::endl;
+	std::cout << std::endl;
+	Data *g = deserialize(serialize());
+	std::cout << g->s1 << std::endl;
+	std::cout << g->s2 << std::endl;
+	std::cout << g->n << std::endl;
+	std::cout << std::endl;
+	Data *t = deserialize(serialize());
+	std::cout << t->s1 << std::endl;
+	std::cout << t->s2 << std::endl;
+	std::cout << t->n << std::endl;
+	std::cout << std::endl;
+	Data *h = deserialize(serialize());
+	std::cout << h->s1 << std::endl;
+	std::cout << h->s2 << std::endl;
+	std::cout << h->n << std::endl;
 	return (0);
 }
