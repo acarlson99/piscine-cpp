fe6a57cfc5c25a8f422272e7a2e7636194a57eb0
Alexander Carlson <acarlson@e1z3r2p8.42.us.org>
2019-02-01 16:58:49 -0800

    uh

--- day08/ex02/mutantstack.hpp
+++ day08/ex02/mutantstack.hpp
@@ -6,7 +6,7 @@
 //   By: acarlson <marvin@42.fr>                    +#+  +:+       +#+        //
 //                                                +#+#+#+#+#+   +#+           //
 //   Created: 2019/02/01 15:28:49 by acarlson          #+#    #+#             //
-//   Updated: 2019/02/01 16:38:15 by acarlson         ###   ########.fr       //
+//   Updated: 2019/02/01 16:56:10 by acarlson         ###   ########.fr       //
 //                                                                            //
 // ************************************************************************** //
 
@@ -19,7 +19,7 @@
 # include <list>
 
 template<typename T>
-class MutantStack: public std::stack<T> {
+class MutantStack<T>: public std::stack<T> {
 
 public:
 	MutantStack( void );
@@ -32,11 +32,9 @@ public:
 			this->_stack.push_back(n);
 		}
 
-	MutantStack			pop( void )
+	void				pop( void )
 		{
-			std::list<T>	tmp = this->_stack;
-			this->_stack = this->_stack.next;
-			return (tmp);
+			return (this->_stack.pop_front());
 		}
 
 	// std::iterator<T>		begin( void )
